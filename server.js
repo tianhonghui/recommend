042686f3fd1677f0d2630a260fc3473f9c09d9a4
tianhonghui <tianhonghui@meituan.com>
2014-05-15 16:51:08 +0800

    增加接口参数appid，传入分配的appid返回的结果中去除自己的app

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-var http = require('http');
 var express = require('express');
-var fs = require('fs');
 var mongoose = require('mongoose');
 //连接到recommend这个数据库
 mongoose.connect('mongodb://localhost/recommend');
@@ -14,9 +12,10 @@ db.once('open',function callback() {
 
 //定义一个Schema，一个Schema的意思是一个数据模型，就是关系映射的规则
 var appSchema = mongoose.Schema({
-	name: String,
-	description : String,
-	appid : String,
+	name: String, 			//应用名称
+	description : String,  	//描述
+	appid : String,			//应用id，由推荐系统分配
+	marketid : String 		//市场的marketid
 });
 
 //将recommend数据库中app映射到前面定义的appSchema上
@@ -26,8 +25,18 @@ var db = mongoose.model('app',appSchema);
 var app = express();
 
 app.get('/recommend',function(req,res) {
-	db.find({},function(err,app){
-		res.send({applist:app});
+	//获取query串中的appid值
+	var appid = req.query.appid;
+	
+	db.find({},function(err,apps){
+		//创建一个结果列表
+		var result = new Array();
+		//过滤掉调用appid相同的数据
+		apps.forEach(function(app){
+			if(app.appid != appid)
+				result.push(app);
+		});
+		res.send({applist:result});
 	});
 });
 
